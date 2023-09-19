88daf239806b0f49b34621b48b4dbe8f60a9e4cd
samuel.abreu <samuel.abreu@fleetcor.com.br>
2023-09-19 17:07:50 -0300

    feat: initializing project

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,37 +1,34 @@
 class Customer {
 
   _id: string;
-  _name: string;
-  _address: string;
+  _name: string = "";
+  _address: string = "";
+  _active: boolean = true;
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string, name: string) {
     this._id = id;
     this._name = name;
-    this._address = address;
   }
 
-  get id(): string {
-    return this._id;
+  validate() {
+    if (this._name === "") {
+      throw new Error("Name is required");
+    }
+    if(this._id.length === 0) {
+      throw new Error("ID is required");
+    }
   }
 
-  get name(): string {
-    return this._name;
-  }
-
-  get address(): string {
-    return this._address;
-  }
-
-  set id(id: string) {
-    this._id = id;
-  }
-
-  set name(name: string) {
+  changeName(name: string) {
     this._name = name;
+    this.validate();
   }
 
-  set address(address: string) {
-    this._address = address;
+  activate() {
+    this._active = true;
   }
 
+  desactivate() {
+    this._active = false;
+  }
 }
